2a63fd3c9fc84341532433deee72d9133964bf47
Maryna Markova <markova.maryna@gmail.com>
2023-04-19 19:46:03 +0300

    add test to main package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,7 +20,26 @@ var session *scs.SessionManager
 
 // main is the main application function
 func main() {
-	// what am I going to put in the session
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	
+	srv := &http.Server {
+	Addr: 		portNumber,
+	Handler: 	routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+	log.Fatal(err)
+	}
+}
+
+func run() error {
+// what am I going to put in the session
 	gob.Register(models.Reservation{})
 
 	// change this to true when in production
@@ -38,6 +57,7 @@ func main() {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -47,18 +67,7 @@ func main() {
 	handlers.NewHandlers(repo)
 
 	render.NewTemplates(&app)
-
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-	
-	srv := &http.Server {
-	Addr: 		portNumber,
-	Handler: 	routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	if err != nil {
-	log.Fatal(err)
-	}
+	return nil
 }
 
 //To run the program: go run ./cmd/web/.
