c5bf477f271d2bcf03786cd85e438bf67682400a
Maryna Markova <markova.maryna@gmail.com>
2023-04-27 19:21:41 +0300

    add error handling to a helpers package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/MarynaMarkova/Go-bookings/internal/config"
 	"github.com/MarynaMarkova/Go-bookings/internal/handlers"
+	"github.com/MarynaMarkova/Go-bookings/internal/helpers"
 	"github.com/MarynaMarkova/Go-bookings/internal/models"
 	"github.com/MarynaMarkova/Go-bookings/internal/render"
 	"github.com/alexedwards/scs/v2"
@@ -17,6 +19,8 @@ import (
 const portNumber = ":8080"
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main application function
 func main() {
@@ -45,6 +49,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog 
+
 	// set up the session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
@@ -65,8 +75,9 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
+
 	return nil
 }
 
