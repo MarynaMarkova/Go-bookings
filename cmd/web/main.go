92285bdba474a7ab3c7524daf5c7523a1cf006f5
Maryna Markova <markova.maryna@gmail.com>
2023-05-14 00:07:14 +0300

    Insert Room Restrictions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -71,7 +71,7 @@ func run() (*driver.DB, error) {
 
 	// connect to database
 	log.Println("Connecting to database...")
-	db, err := driver.ConnectSQL(`host=localhost port=5432 dbname=bookings user=postgres password=s`)
+	db, err := driver.ConnectSQL(`host=localhost port=5432 dbname=bookings user=postgres password=free23lancer`)
 	// database.yml password
 	if err !=nil {
 		log.Fatal("Cannot connect to database! Dying...")
