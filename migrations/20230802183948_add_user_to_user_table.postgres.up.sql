d5be3f99aee4f969994632b51ee6a84c89ec97f6
Maryna Markova <markova.maryna@gmail.com>
2023-08-02 21:52:30 +0300

    add user migrations

---
+++ migrations/20230802183948_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO "public"."users"("first_name",	"last_name", "email","password", "access_level", "created_at",	"updated_at")
+VALUES(E'Maryna',E'Markova',E'admin@admin.com',E'$2a$12$szptAjHYysMrpkNhW4BILe/8sSzPmToHRtyGIworw0uXcyS5NIoTy',1,E'2023-06-25 00:00:00.000',E'2023-06-25 00:00:00.000')
